a39ddb68a9d233917db1df8cec1447cdef72422c
Adithya Kamath <adithyakamath96@gmail.com>
2019-05-15 01:51:29 +0530

    Added API. Still not a master of styling, hence need to add styling!

--- ComplGen/ComplGen/MainPage.xaml.cs
+++ ComplGen/ComplGen/MainPage.xaml.cs
@@ -1,6 +1,8 @@
-﻿using System;
+﻿using Newtonsoft.Json;
+using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Http;
 using System.Text;
 using System.Threading.Tasks;
 using Xamarin.Forms;
@@ -9,14 +11,36 @@ namespace ComplGen
 {
     public partial class MainPage : ContentPage
     {
+        public class PickupText
+        {
+            public string id { get; set; }
+            public string username { get; set; }
+            public string tweet { get; set; }
+            public string version { get; set; }
+        }
         public MainPage()
         {
             InitializeComponent();
         }
 
-        private void Button_Clicked(object sender, EventArgs e)
+        private async void Button_Clicked(object sender, EventArgs e)
         {
-           
+            using (HttpClient client = new HttpClient())
+            {
+                using (HttpResponseMessage response = await client.GetAsync("http://pebble-pickup.herokuapp.com/tweets/random"))
+                {
+                    using (HttpContent content = response.Content)
+                    {
+                        var mycontent = await content.ReadAsStringAsync();
+                        PickupText someText = JsonConvert.DeserializeObject<PickupText>(mycontent);
+                        Console.WriteLine(someText.tweet);
+                       await DisplayAlert("Compliment", someText.tweet, "Okay");
+                    }
+                }
+
+            }
+            
+
         }
     }
 }
